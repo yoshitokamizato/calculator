8106272fd77e8c91414fabb5fda0ee09bcb60de8
Yoshito Kamizato <yuyueureka157@gmail.com>
2019-05-31 16:09:45 +0900

    Adding files.

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -11,6 +11,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .calculator {
-  
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto);
 }
 </style>
